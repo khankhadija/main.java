c8cb86977e34ef1955bba7f4d4c6451e1c6398a6
khankhadija <72740976+khankhadija@users.noreply.github.com>
2020-10-13 11:14:24 +0100

    Add files via upload

--- Java.java
+++ Java.java
@@ -3,17 +3,17 @@ class Main {
         Person khadija = new Person();
         khadija.name = "Khadija";
         khadija.age = 20;
-        khadija.atHome - true;
-        khadija.initial - 'K';
+        khadija.atHome = true;
+        khadija.initial = 'K';
 
-        System.out.printIn(aboutMe(khadija.name, khadija.age, khadija.atHome, khadija.initial)):
+        System.out.printIn(aboutMe(khadija.name, khadija.age, khadija.atHome, khadija.initial));
     }
 
     public static String aboutMe(String name, int age, boolean atHome, char initial) {
-        String out - name + " is " + Integer.toString(age) + " and has the initial " + Character.toString(initial);
+        String out = name + " is " + Integer.toString(age) + " and has the initial " + Character.toString(initial);
 
         if (atHome == true) {
-            out += " and is at home "
+            out += " and is at home ";
         } else {
             out += " and is not at home. ";
         }
